d24c93ce8286583525d7e7e0b1a0d39ab779674e
chrisloarryn <chrisloarryn@gmail.com>
2021-01-15 18:38:30 -0300

    Description

--- src/components/UI/Insurance/index.tsx
+++ src/components/UI/Insurance/index.tsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import { AppStateContext } from '../../../Providers/AppState';
 import { NoData } from '../NoData/styles';
-import { Card, CardContent, Container, Img } from './styles';
+import { Card, CardContent, Container, Img, Description } from './styles';
 
 export const ActiveInsure = (): JSX.Element => {
   const { active: activeInsure } = useContext(AppStateContext);
@@ -16,7 +16,7 @@ export const ActiveInsure = (): JSX.Element => {
           <h4>
             <b>{activeInsure.name}</b>
           </h4>
-          <p>{activeInsure.description}</p>
+          <Description>{activeInsure.description}</Description>
           <div style={{ display: 'flex', justifyContent: 'center' }}>
             <p style={{ color: 'tomato' }}>Precio:</p>
             <p style={{ color: 'tomato', marginLeft: '5px' }}>
